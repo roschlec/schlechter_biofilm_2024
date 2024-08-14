a823e368a16e2d61b698adf83b88dbe3c024926f
roschlec <roschlec@gmail.com>
2024-08-14 17:49:52 +0200

    update script to install packages from renv

--- src/dependencies.R
+++ src/dependencies.R
@@ -6,3 +6,9 @@ if(!require(renv)){
 }
 
 source("renv/activate.R")
+
+renv::activate()
+
+# Install all packages expect inborutils, which is downloaded 
+# in download_zenodo.R
+renv::install(exclude = "inborutils")
